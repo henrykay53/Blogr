977ac8481b35a766950cf6663ed5b7b582552632
Henry Kolawole <henrykay53@gmail.com>
2021-09-18 20:10:16 +0100

    hamburger, overlay and its content are fully functional

--- blogr.js
+++ blogr.js
@@ -3,19 +3,14 @@ var ham = document.getElementById("hamburger");
 var overlay = document.getElementById("overlay");
 var body = document.body;
 
-// var dropdown = document.getElementById("dropdown");
-// var content = doccument.getElementById("content");
-// overlay.classList.contains("showOverlay");
 
 //When the hamburger menu is clicked it triggers the image changer function
 // This function changes the hmburger icon as well as displays the overlay
-
-
+// Hamburger menu funtion
 ham.addEventListener("click", function() {
 
     var img = document.getElementById("hamburger");
   
-
     if (overlay.classList.contains("show")) { //hamberger is closed
         overlay.classList.remove("show");
         img.src="images/icon-hamburger.svg";
@@ -23,8 +18,6 @@ ham.addEventListener("click", function() {
         body.classList.remove("noscroll");
         // document.body.style.position = "";
         
-       
-
     } else {
         overlay.classList.add("show"); // hamburger is open
         img.src = "images/icon-close.svg";
@@ -34,27 +27,49 @@ ham.addEventListener("click", function() {
 
     }
 
-    // dropdown.addEventListener("click", function() {
-
-        // if(content.classList.contains("show")) {
-            // content.classList.remove("show");
-            // content.style.display = "none";
-        // } else {
-            // content.classList.add("show");
-            // content.style.display = "block";
-        // }
-    // });
-    
-
 });  
 
 
 
+// Function for overlay content
+let drop1 = document.getElementById("mobile-dropdown1");
+let cont1 = document.getElementById("content1");
+
+let drop2 = document.getElementById("mobile-dropdown2");
+let cont2 = document.getElementById("content2");
+
+let drop3 = document.getElementById("mobile-dropdown3");
+let cont3 = document.getElementById("content3");
 
 
+drop1.addEventListener("click", function() {
+    if(cont1.classList.contains("show")){
+        cont1.classList.remove("show");
+        cont1.style.display = "none";
+    }else{
+        cont1.classList.add("show");
+        cont1.style.display = "block";
+    }
+});
 
 
+drop2.addEventListener("click", function() {
+    if(cont2.classList.contains("show")){
+        cont2.classList.remove("show");
+        cont2.style.display = "none";
+    }else{
+        cont2.classList.add("show");
+        cont2.style.display = "block";
+    }
+});
 
 
-
-
+drop3.addEventListener("click", function() {
+    if(cont3.classList.contains("show")){
+        cont3.classList.remove("show");
+        cont3.style.display = "none";
+    }else{
+        cont3.classList.add("show");
+        cont3.style.display = "block";
+    }
+});
